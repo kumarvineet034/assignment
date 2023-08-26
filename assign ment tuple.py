4f6981acd899631ba7237b24d9ea36acede3a8ae
kumarvineet034 <140829809+kumarvineet034@users.noreply.github.com>
2023-08-25 22:05:20 -0700

    Add files via upload

---
+++ assign ment tuple.py
@@ -0,0 +1,7 @@
+def sort_by_last_element(tuples_list):
+    sorted_list = sorted(tuples_list, key=lambda x: x[-1])
+    return sorted_list
+
+sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+result = sort_by_last_element(sample_list)
+print(result)
